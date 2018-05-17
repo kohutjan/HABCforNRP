103eaec33c9d14dd1c257810f66b68670c6e0283
Honza <kohut.honza@seznam.cz>
2018-05-17 09:34:30 +0200

    eigen download

--- scripts/download_eigen.sh
+++ scripts/download_eigen.sh
@@ -1,7 +1,7 @@
-rm -f ../Eigen.tar.gz
-wget https://www.dl.dropboxusercontent.com/s/hhynuknplh3gwgp/Eigen.tar.gz -P ../include/
-rm -rf ../Eigen
-mkdir ../include/eigen3/
-tar xzf ../include/Eigen.tar.gz -C ../include/eigen3/
-rm -f ../include/Eigen.tar.gz
+rm -f Eigen.tar.gz
+wget https://www.dl.dropboxusercontent.com/s/hhynuknplh3gwgp/Eigen.tar.gz -P include/
+rm -rf include/eigen3/
+mkdir include/eigen3/
+tar xzf include/Eigen.tar.gz -C include/eigen3/
+rm -f include/Eigen.tar.gz
 
